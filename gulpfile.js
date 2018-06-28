6aa316518d7036a332b9302ca15283530f288a57
ixiryyus <julienbonnanfant@gmail.com>
2018-06-28 19:47:03 +0200

    page de contact

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ var watch = require("gulp-watch");
 var concatCss = require('gulp-concat-css');
 var gulp = require("gulp");
 var postcss = require("gulp-postcss");
+var htmllint = require('gulp-htmllint');
+var fancyLog = require('fancy-log');
+var colors = require('ansi-colors');
+
 
 gulp.task("css", function() {
     return gulp
@@ -28,6 +32,25 @@ gulp.task("watch",function() {
     watch("./src/css/*.css", function () {
         return gulp.start('css');
     });
+    watch("./index.html", function () {
+        return gulp.start('html');
+    });
 });
 
-gulp.task("default",['css','watch']);
+gulp.task("default",['css','watch','html']);
+
+gulp.task('html', function() {
+    return gulp.src('./index.html')
+        .pipe(htmllint({}, htmllintReporter));
+});
+
+
+function htmllintReporter(filepath, issues) {
+    if (issues.length > 0) {
+        issues.forEach(function (issue) {
+            fancyLog(colors.cyan('[gulp-htmllint] ') + colors.white(filepath + ' [' + issue.line + ',' + issue.column + ']: ') + colors.red('(' + issue.code + ') ' + issue.msg));
+        });
+
+        process.exitCode = 1;
+    }
+}
