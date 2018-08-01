adacbadbec526e9665fbc4ae579bd8eb62365dfa
ixiryyus <julienbonnanfant@gmail.com>
2018-08-01 17:36:55 +0200

    eyes

--- src/app.js
+++ src/app.js
@@ -64,3 +64,106 @@ window.addEventListener('popstate',function(){
     var url = window.location.href;
     gotopage(url.split('#')[1])
 })
+
+
+
+onmousemove = function moveEyes(e){
+    var left_eye = document.querySelector("#eye-left");
+    var right_eye = document.querySelector("#eye-right");
+    var position_lefteye = left_eye.getBoundingClientRect();
+    var position_righteye = right_eye.getBoundingClientRect();
+    // console.log("mouse location:", e.clientX, e.clientY);
+    // console.log("left location:", position_lefteye.x, position_lefteye.y);
+    // console.log("right location:", position_righteye.x, position_righteye.y);
+
+
+
+    // Limite maximum definie
+    const maxEyeMoveX = 7;
+    const maxEyeMoveY = 4;
+
+
+    var documentHeight = document.querySelector('body').clientHeight;
+    var documentWidth = document.querySelector('body').clientWidth;
+
+
+
+    var eyeleft_x_diff = position_lefteye.x - e.clientX;
+    var eyeleft_y_diff = position_lefteye.y - e.clientY;
+
+    var eyeright_x_diff = position_righteye.x - e.clientX;
+    var eyeright_y_diff = position_righteye.y - e.clientY;
+
+
+
+
+
+    // On calcule la distance maximum por la sourie entre l'oeil et le bord de l'ecran a gauche et a droite
+    var eyeLeftMaxMouseMoveOnYPositive = documentHeight - position_lefteye.y ;
+    var eyeLeftMaxMouseMoveOnYNegative = position_lefteye.y ;
+
+    var eyeLeftMaxMouseMoveOnXPositive = documentWidth -  position_lefteye.x;
+    var eyeLeftMaxMouseMoveOnXNegative = position_lefteye.x;
+
+
+    var eyeRightMaxMouseMoveOnYPositive = documentHeight - position_righteye.y ;
+    var eyeRightMaxMouseMoveOnYNegative = position_righteye.y ;
+
+    var eyeRightMaxMouseMoveOnXPositive = documentWidth -  position_righteye.x;
+    var eyeRightMaxMouseMoveOnXNegative = position_righteye.x;
+
+
+console.log('eyeRightMaxMouseMoveOnYPositive',eyeRightMaxMouseMoveOnYPositive);
+console.log('eyeRightMaxMouseMoveOnYNegative',eyeRightMaxMouseMoveOnYNegative);
+
+    // On regarde si la sourie est entre les deux yeux
+    // if(position_lefteye.x < e.clientX &&  e.clientX <  position_righteye.x ){
+    //     var eyeLeftMaxMouseMoveOnXPositive = (position_righteye.x - position_lefteye.x) /1.5  ;
+    //     var eyeRightMaxMouseMoveOnXNegative = (position_righteye.x - position_lefteye.x) /1.5  ;
+    // }
+
+
+
+    // Initalisation d nombre de pixel a deplacer
+    var translate_eyeleft_X = 0;
+    var translate_eyeleft_Y = 0;
+
+
+    // Calcul du ratio a deplacer en fonction du Limite maximum definie
+    if( eyeleft_x_diff > 0) translate_eyeleft_X = (maxEyeMoveX * eyeleft_x_diff) / eyeLeftMaxMouseMoveOnXNegative ;
+    else                    translate_eyeleft_X = (maxEyeMoveX * eyeleft_x_diff) / eyeLeftMaxMouseMoveOnXPositive ;
+
+    if( eyeleft_y_diff > 0) translate_eyeleft_Y = (maxEyeMoveY * eyeleft_y_diff) / eyeLeftMaxMouseMoveOnYPositive ;
+    else                    translate_eyeleft_Y = (maxEyeMoveY * eyeleft_y_diff) / eyeLeftMaxMouseMoveOnYNegative ;
+
+
+
+
+
+
+
+    if( eyeright_x_diff > 0) translate_eyeright_X = (maxEyeMoveX * eyeright_x_diff) / eyeRightMaxMouseMoveOnXNegative ;
+    else                    translate_eyeright_X = (maxEyeMoveX * eyeright_x_diff) / eyeRightMaxMouseMoveOnXPositive ;
+
+    if( eyeright_y_diff > 0) translate_eyeright_Y = (maxEyeMoveY * eyeright_y_diff) / eyeRightMaxMouseMoveOnYPositive ;
+    else                    translate_eyeright_Y = (maxEyeMoveY * eyeright_y_diff) / eyeRightMaxMouseMoveOnYNegative ;
+
+
+
+
+
+
+
+    left_eye.style.transform = "translate("+ -translate_eyeleft_X +"px,"+ -translate_eyeleft_Y +"px)";
+    right_eye.style.transform = "translate("+ -translate_eyeright_X +"px,"+ -translate_eyeright_Y +"px)";
+
+
+
+
+
+
+    // var translate_X = Math.random() * maxEyeMoveX;
+    // var translate_Y = Math.random() * maxEyeMoveY;
+
+
+}
